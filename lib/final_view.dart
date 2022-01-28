296103e8c74dc2ddd633c850880b87d7ac51cd9e
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2022-01-29 01:22:03 +0530

    Add project part

---
+++ lib/final_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FinalView extends StatefulWidget {
+  const FinalView({Key? key}) : super(key: key);
+
+  @override
+  _FinalViewState createState() => _FinalViewState();
+}
+
+class _FinalViewState extends State<FinalView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
