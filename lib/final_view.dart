abd81f1058955790800649cb002d0aa8a29b9e03
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2022-01-31 18:17:16 +0530

    change some plugins

--- lib/final_view.dart
+++ lib/final_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_pdf/pdf.dart';
 
 class FinalView extends StatefulWidget {
   const FinalView({Key? key}) : super(key: key);
@@ -11,7 +12,31 @@ class _FinalViewState extends State<FinalView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.picture_as_pdf,
+                size: 100,
+              ),
+              Text(
+                "Genarate PDF",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () async {},
+                child: Text("Invoice PDF"),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
