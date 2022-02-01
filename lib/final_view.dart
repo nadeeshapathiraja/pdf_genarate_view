62cf70c284c6d6372040b4d5c8cc79863cb92a5e
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2022-02-01 12:07:10 +0530

    add text and images

--- lib/final_view.dart
+++ lib/final_view.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:pdf_vie/model/final_view_model.dart';
 import 'package:syncfusion_flutter_pdf/pdf.dart';
-
+import 'dart:typed_data';
+import 'package:flutter/services.dart' show rootBundle;
 import 'provider/final_vie_provider.dart';
 
 class FinalView extends StatefulWidget {
@@ -43,13 +44,41 @@ class _FinalViewState extends State<FinalView> {
     );
   }
 
+//Text add
   Future<void> _createPdf() async {
     PdfDocument document = PdfDocument();
-    document.pages.add();
+    final page = document.pages.add();
+
+    page.graphics.drawString(
+      "Welcome to My World",
+      PdfStandardFont(
+        PdfFontFamily.helvetica,
+        30,
+      ),
+    );
+//Image Add
+    page.graphics.drawImage(
+      PdfBitmap(
+        await _readImageData("log.png"),
+      ),
+      const Rect.fromLTWH(
+        0,
+        30,
+        440,
+        550,
+      ),
+    );
+//table add
+    PdfGrid
 
     List<int> bytes = document.save();
     document.dispose();
 
     saveAndLaunchFile(bytes, 'Invoice.pdf');
   }
+}
+
+Future<Uint8List> _readImageData(String name) async {
+  final data = await rootBundle.load('assets/images/$name');
+  return data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
 }
